85aefc3d86a851c8a9dcbbae6fbb2c4fe874a68a
Julien Houle <julien.houle@hotmail.com>
2021-04-02 17:41:40 -0400

    Update main.py

--- main.py
+++ main.py
@@ -34,3 +34,33 @@ def Crank_Nico():
 # for i in range(10000):
     # print(psi(i,100))
 
+def Thomas(Matrice, Vecteur):
+    '''
+    Fonction qui utilise l'algo de Thomas pour résoudre AX = v
+
+    Paramètres: Matrice est un matrice carré tridiagonale, Vecteur est une matrice vecteur
+
+    Retourne: Un vecteur correspondant à X
+
+    NOTE: Jesus saith unto him, Thomas, because thou hast seen me, thou hast believed:
+    blessed are they that have not seen, and yet have believed
+    '''
+    taille = len(Matrice)
+    noVect = np.empty([taille,1])
+    # Boucle qui fait la réduction de Gauss simplifiée sur la matrice et le vecteur.
+    for i in range(taille-1):
+        Vecteur[i][0] = Vecteur[i][0]/ Matrice[i][i]
+        Matrice[i] = Matrice[i]/ Matrice[i][i]
+        Vecteur[i+1][0] = Vecteur[i+1][0] - Matrice[i+1][i]*Vecteur[i][0]
+        Matrice[i+1] = Matrice[i+1]-(Matrice[i+1][i]* Matrice[i])
+    Vecteur[taille-1][0] = Vecteur[taille-1][0]/ Matrice[taille-1][taille-1]
+    Matrice[taille-1] = Matrice[taille-1] / Matrice[taille-1][taille-1]
+    noVect[taille - 1][0] = Vecteur[taille - 1][0]
+    # Boucle qui construit notre vecteur de sortie.
+    for i in reversed(range(taille-1)):
+        noVect[i][0] = Vecteur[i][0] - Matrice[i][i+1]*noVect[i+1][0]
+    return noVect
+
+test = np.array([[2.0,6,0, 0], [9,7,3, 0], [0,9,6, 6], [0,0,1,9]])
+Vecteur = np.array([[1.000], [2], [54], [0]])
+print(Thomas(test, Vecteur))
