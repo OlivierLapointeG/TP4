260b74bdd834b5e6c559e4043017830e2433c6d9
Olivier Lapointe-Gagne <olivier.lapointe-gagne.1@ulaval.ca>
2021-04-02 16:08:42 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,16 @@ import numpy as np
 import matplotlib.pyplot as plt 
 from math import e 
 import time 
+from scipy.constants import hbar
 
 
 
-def psi(x, L):
+'''
+Liste de constantes utiles
+'''
+m_e = 9.109e-31 # kg 
+
+def psi_0(x, L):
     '''
     Fonction qui calcule la fonction d'onde d'un électron au temps 0 à une position donné
 
@@ -19,6 +25,36 @@ def psi(x, L):
     psi = e**(-(x-x_0)**2/(2*sig**2))*e**(1j*k*x)
     return psi
 
+def matrice(lettre,ran,col,N,L,h):
+    '''
+    Fonction crée la matrice A ou B
+
+    Paramètres: lettre: choix de matrice à créer, ran:nombre de rangées, col:nombre de colonnes, 
+                N:nombre d'itérations positionnelles, L: longueur de la boîte, h:grandeur des itérations temporelles
+
+    Retourne: une matrice tridiagonale qui constitue notre système équation différentielle
+    '''
+    a = L/N
+    a_1 = 1 + h*1j*hbar/(2*m_e*a**2)
+    a_2 = -h*1j*hbar/(4*m_e*a**2)
+    b_1 = 1 - h*1j*hbar/(2*m_e*a**2)
+    b_2 = h*1j*hbar/(4*m_e**2)
+    matrice = np.zeros((ran,col),complex)
+    if lettre == 'A':
+        for i in range(ran):
+            for l in range(col):
+                if i == l:
+                    matrice[i][l]  = a_1
+                if i == l + 1 or i == l -1:
+                    matrice[i][l] = a_2
+    if lettre == 'B':
+        for i in range(ran):
+            for l in range(col):
+                if i == l:
+                    matrice[i][l]  = b_1
+                if i == l + 1 or i == l -1:
+                    matrice[i][l] = b_2
+    return matrice
 
 
 def Crank_Nico():
@@ -30,5 +66,3 @@ def Crank_Nico():
     Retourne:
     '''
 
-for i in range(10000):
-    print(psi(i,100))
