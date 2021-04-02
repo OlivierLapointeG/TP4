a5273b75a9c80201e7438f96e58e640077e11ae3
Olivier Lapointe-Gagne <olivier.lapointe-gagne.1@ulaval.ca>
2021-04-02 16:15:45 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import numpy as np
-import matplotlib.pyplot as plt 
-from math import e 
-import time 
+import matplotlib.pyplot as plt
+from math import e
+import time
+from scipy.constants import hbar
 
 
 
-def psi(x, L):
+'''
+Liste de constantes utiles
+'''
+m_e = 9.109e-31 # kg 
+
+def psi_0(x, L):
     '''
     Fonction qui calcule la fonction d'onde d'un électron au temps 0 à une position donné
 
@@ -19,9 +25,53 @@ def psi(x, L):
     psi = e**(-(x-x_0)**2/(2*sig**2))*e**(1j*k*x)
     return psi
 
+def matrice(lettre,ran,col,N,L,h):
+    '''
+    Fonction crée la matrice A ou B
+
+    Paramètres: lettre: choix de matrice à créer, ran:nombre de rangées, col:nombre de colonnes, 
+                N:nombre d'itérations positionnelles, L: longueur de la boîte, h:grandeur des itérations temporelles
+
+    Retourne: une matrice tridiagonale qui constitue notre système équation différentielle
+    '''
+    a = L/N
+    a_1 = 1 + h*1j*hbar/(2*m_e*a**2)
+    a_2 = -h*1j*hbar/(4*m_e*a**2)
+    b_1 = 1 - h*1j*hbar/(2*m_e*a**2)
+    b_2 = h*1j*hbar/(4*m_e**2)
+    matrice = np.zeros((ran,col),complex)
+    if lettre == 'A':
+        for i in range(ran):
+            for l in range(col):
+                if i == l:
+                    matrice[i][l]  = a_1
+                if i == l + 1 or i == l -1:
+                    matrice[i][l] = a_2
+    if lettre == 'B':
+        for i in range(ran):
+            for l in range(col):
+                if i == l:
+                    matrice[i][l]  = b_1
+                if i == l + 1 or i == l -1:
+                    matrice[i][l] = b_2
+    return matrice
 
 
-def Crank_Nico():
+def psi_0_vec(L,N):
+    '''
+    Fonction qui construit le vecteur psi(0) en fonction des pas de distance a
+
+    Paramètres : L, la longueur de la boîte unidimensionnelle, N, le nombre de pas dans la boîte
+
+    Retourne : le vecteur psi(0)
+    '''
+    a = L/N
+    psi0 = np.empty([N,1],complex)
+    for i in range(N):
+        psi0[i]=psi_0(i*a,L)
+    return psi0
+
+def Crank_Nico(h,N):
     '''
     Fonction qui estime la valeur de psi en fonction du temps et de x avec la méthode de Crank-Nicolson
 
@@ -30,5 +80,3 @@ def Crank_Nico():
     Retourne:
     '''
 
-for i in range(10000):
-    print(psi(i,100))
