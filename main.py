524d74e4642d4daea131d39398da52d2a6294c81
Olivier Lapointe-Gagne <olivier.lapointe-gagne.1@ulaval.ca>
2021-04-02 15:10:29 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,34 @@
 import numpy as np
 import matplotlib.pyplot as plt 
+from math import e 
 import time 
+
+
+
+def psi(x, L):
+    '''
+    Fonction qui calcule la fonction d'onde d'un électron au temps 0 à une position donné
+
+    Paramètres: x: La position de l'électron, L: La longueur de la boîte
+
+    Retourne: La valeur de psi 
+    '''
+    x_0 = L/2
+    sig = 1e-10 #en mètres
+    k = 5e10 # en 1/mètres
+    psi = e**(-(x-x_0)**2/(2*sig**2))*e**(1j*k*x)
+    return psi
+
+
+
+def Crank_Nico():
+    '''
+    Fonction qui estime la valeur de psi en fonction du temps et de x avec la méthode de Crank-Nicolson
+
+    Paramètres:
+
+    Retourne:
+    '''
+
+for i in range(10000):
+    print(psi(i,100))
